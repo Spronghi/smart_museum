df9d0c6ed2f6b97534968d65f4533344c8031830
spronghi <sim.colaci@gmail.com>
2017-12-29 18:50:02 +0100

    Using service to mantein the currentOperator that log in
    I am not pretty sure about which way is the best one.

--- src/main/resources/static/home/scripts/controllers/LoginCtrl.js
+++ src/main/resources/static/home/scripts/controllers/LoginCtrl.js
@@ -1,14 +1,19 @@
 angular
   .module('HomeApp', [])
-  .controller('LoginCtrl', ['$scope', '$rootScope', '$http', '$window', function ($scope, $rootScope, $http, $window) {
+  .service('loginService', function() {
+    this.currentOperator = [];
+  })
+  .controller('LoginCtrl', ['$scope', 'loginService', '$http', '$window', function ($scope, loginService, $http, $window) {
       $scope.currentOperator = [];
       $scope.login = function () {
          $http.get("/user/login/"+$scope.vm.username)
             .then(function (res) {
             $scope.currentOperator = res.data;
             if($scope.currentOperator.username == $scope.vm.username){
-              $rootScope.currentOperator = $scope.currentOperator;
-              $window.location.href = "/backoffice/index.html"; 
+              loginService.currentOperator = $scope.currentOperator;
+              $window.location.href = "/backoffice/index.html";
+            } else {
+              $scope.userLabel = "Invalid username or password";
             }
         });
       }
---
+++ src/main/resources/static/home/scripts/services/LoginService.js
@@ -0,0 +1,5 @@
+angular
+  .module('HomeApp', [])
+  .factory('loginService', function() {
+    this.currentOperator = [];
+  });
