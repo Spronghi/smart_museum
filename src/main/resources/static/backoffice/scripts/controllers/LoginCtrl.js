cb1fcf9d6587b1d4bc04b7d8efccf380e7a5da90
spronghi <sim.colaci@gmail.com>
2018-01-08 16:09:31 +0100

    Divided operator page from admin page

--- src/main/resources/static/backoffice/scripts/controllers/LoginCtrl.js
+++ src/main/resources/static/backoffice/scripts/controllers/LoginCtrl.js
@@ -1,15 +1,19 @@
-app.controller('LoginCtrl', function ($scope, loginService, $http, $window) {
-      $scope.currentOperator = [];
-      $scope.login = function () {
-         $http.get("/user/login/"+$scope.vm.username)
+app.controller('LoginCtrl', function ($scope, loginService, operatorService, $http, $window) {
+    $scope.currentOperator = [];
+    $scope.login = function () {
+        $http.get("/user/login/"+$scope.vm.username)
             .then(function (res) {
-            $scope.currentOperator = res.data;
-            if($scope.currentOperator.username == $scope.vm.username){
-              loginService.set($scope.currentOperator);
-              $window.location.href = "/backoffice/index.html";
-            } else {
-              $scope.userLabel = "Invalid username or password";
-            }
-        });
-      }
-  });
+                $scope.currentOperator = res.data;
+                if($scope.currentOperator.username == $scope.vm.username){
+                    loginService.set($scope.currentOperator);
+                    if($scope.currentOperator.admin == true){
+                        $window.location.href = "/backoffice/admin.html";
+                    } else {
+                        $window.location.href = "/backoffice/index.html";
+                    }
+                } else {
+                    $scope.userLabel = "Invalid username or password";
+                }
+            });
+        }
+    });
