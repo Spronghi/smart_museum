d273e5fd0d8905d426e7206b3e68c7dfc30e98eb
spronghi <sim.colaci@gmail.com>
2018-01-08 10:04:21 +0100

    Removed ngRoute and added ngCookie
    All the service modules are now registered in app.service. I am considering to change 'BackofficeApp' in 'app'

--- src/main/resources/static/backoffice/scripts/app.js
+++ src/main/resources/static/backoffice/scripts/app.js
@@ -1,9 +1,1 @@
-angular.module('BackofficeApp', ['ngRoute'])
-    .config(['$routeProvider', function ($routeProvider) {
-	    $routeProvider
-	            .when('/more:id', {
-	            templateUrl: 'template.html'
-	            //.otherwise({
-	            //    redirectTo: '/'
-	        });
-    }]);
+var app = angular.module('BackofficeApp',['app.services']);
