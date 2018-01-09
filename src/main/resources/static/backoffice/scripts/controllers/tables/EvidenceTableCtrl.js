25c835fb9c27e1e98f68b32c58aff285077b67e9
spronghi <sim.colaci@gmail.com>
2018-01-09 15:55:57 +0100

    Table pages now have their own controller

--- src/main/resources/static/backoffice/scripts/controllers/tables/EvidenceTableCtrl.js
+++ src/main/resources/static/backoffice/scripts/controllers/tables/EvidenceTableCtrl.js
@@ -1,4 +1,4 @@
-app.controller('EvidenceCtrl', function ($scope, $http, loginService) {
+app.controller('EvidenceTableCtrl', function ($scope, $http, loginService) {
     $http.get("/user/evidences_by_operator/"+loginService.getCurrentId())
         .then(function (res) {
             $scope.evidences = res.data;
